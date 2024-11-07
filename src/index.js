c040d1be0cf6bc3f4dca7a1bf7a6f61eb88a2688
kylepunta <kylepurcell1998@gmail.com>
2024-11-07 16:52:39 +0000

    Imported all three modular functions; added event listeners to each nav button for loading the respective contents

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 import "./styles.css";
+import { loadHomePage } from "./home-page";
+import { loadMenuPage } from "./menu-page";
 import { loadContactPage } from "./contact-page";
-loadContactPage();
+
+loadHomePage();
+
+const content = document.querySelector('#content');
+const buttons = document.querySelectorAll('button');
+const homeBtn = buttons[0];
+const menuBtn = buttons[1];
+const contactBtn = buttons[2];
+
+homeBtn.addEventListener('click', function () {
+    content.innerHTML = '';
+    loadHomePage();
+});
+
+menuBtn.addEventListener('click', function () {
+    content.innerHTML = '';
+    loadMenuPage();
+});
+
+contactBtn.addEventListener('click', function () {
+    content.innerHTML = '';
+    loadContactPage();
+});
+
+function setActiveButton(){
+    
+};
+
+buttons.forEach((button) => {
+    button.addEventListener('click', function () {
+
+    })
+});
