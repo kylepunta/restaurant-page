e4823b39b57ddaacc0c415d44d2e259336d66242
kylepunta <kylepurcell1998@gmail.com>
2024-11-08 13:00:02 +0000

    Added function to initialize home button as the active button; added function to set the active button; added event listener for each button with the set active button function

--- src/index.js
+++ src/index.js
@@ -3,35 +3,43 @@ import { loadHomePage } from "./home-page";
 import { loadMenuPage } from "./menu-page";
 import { loadContactPage } from "./contact-page";
 
-loadHomePage();
-
 const content = document.querySelector('#content');
 const buttons = document.querySelectorAll('button');
 const homeBtn = buttons[0];
 const menuBtn = buttons[1];
 const contactBtn = buttons[2];
 
-homeBtn.addEventListener('click', function () {
+homeBtn.addEventListener('click', function() {
     content.innerHTML = '';
     loadHomePage();
 });
 
-menuBtn.addEventListener('click', function () {
+menuBtn.addEventListener('click', function() {
     content.innerHTML = '';
     loadMenuPage();
 });
 
-contactBtn.addEventListener('click', function () {
+contactBtn.addEventListener('click', function() {
     content.innerHTML = '';
     loadContactPage();
 });
 
-function setActiveButton(){
-    
+function initActiveButton(){
+    homeBtn.classList.add('active');
+};
+
+function setActiveButton(button){
+    buttons.forEach((button) => {
+        button.classList.remove('active');
+    });
+    button.classList.add('active');
 };
 
 buttons.forEach((button) => {
-    button.addEventListener('click', function () {
-
+    button.addEventListener('click', function() {
+        setActiveButton(button);
     })
 });
+
+loadHomePage();
+initActiveButton();
